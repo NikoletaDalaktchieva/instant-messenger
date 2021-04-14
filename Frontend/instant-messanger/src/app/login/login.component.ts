4779c80b852afc27e77787e9c12f557e4358dd15
Николета Далакчиева <nikoletadal@gmail.com>
2021-04-14 16:54:39 +0300

    Google authenticator

--- Frontend/instant-messanger/src/app/login/login.component.ts
+++ Frontend/instant-messanger/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SocialAuthService, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,14 +11,37 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   title = 'Instant messanger';
-  constructor(private router: Router) {
+  socialUser: SocialUser = new SocialUser;
+
+  constructor(private router: Router,
+    private formBuilder: FormBuilder,
+    private socialAuthService: SocialAuthService,
+  ) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+    });
   }
+
 
   loginUser(email: string, password: string) {
     //alert(email + '\n' + password);
     this.router.navigateByUrl('chat');
   }
+
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  logOut(): void {
+    this.socialAuthService.signOut();
+  }
+
 }
