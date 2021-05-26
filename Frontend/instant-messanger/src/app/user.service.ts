4dfbd1c20727999483165d9cba997be267577a23
Николета Далакчиева <nikoletadal@gmail.com>
2021-05-26 15:23:48 +0300

    login server logic

--- Frontend/instant-messanger/src/app/user.service.ts
+++ Frontend/instant-messanger/src/app/user.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from "@angular/common/http";
 
 import { Router } from '@angular/router';
 import { AppComponent } from "./app.component";
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -22,32 +21,17 @@ export class UserService {
         subscribe(
           response => {
             result = response;
-            if (result.result == "OK") {
+            if (result.result == 1) {
               this.router.navigateByUrl('chat');
+            } else {
+              console.log(result.message);
+              AppComponent.showError(result.message);
             }
           },
-          error => { this.handleError(error); },
+          error => { AppComponent.showError(); },
           () => { }
         );
     });
 
   }
-
-
-  handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:' + error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${JSON.stringify(error)}`);
-    }
-    // return an observable with a user-facing error message
-    // return throwError(
-    //   'Something bad happened; please try again later.');
-  }
-
 }
