8ea94cdf87e7f7915a2dd9f241a14d2b25261d76
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-05-28 23:23:34 +0300

    Change some configurations

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  new Promise((resolve, reject) => {
+    res.json({result:"OK"});
+  });
+});
+//localhost:8080/user/ -> OK
+// install express,cors
+
 router.post('/login', (req, res) => {
   new Promise((resolve, reject) => {
     console.log(new Date().toLocaleString())
