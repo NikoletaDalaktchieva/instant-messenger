8f9a5df0acf8fd6e41f942e41f57b70310436068
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-01 22:21:37 +0300

    Created chat router

---
+++ Backend/routes/chatRouter.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+//const MongoClient = require('mongodb').MongoClient;
+const chatController = require("../controllers/chatController");
+
+module.exports.chatRouter = router;
