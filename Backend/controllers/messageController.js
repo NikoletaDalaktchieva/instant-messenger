dc5bdabb89360838de8437e7c880832cdbe244d3
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-01 22:41:18 +0300

    Tested loading list of messages

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -1,1 +1,12 @@
 const Message = require('../models/message');
+
+exports.getMessage = function (req, res) {
+    Message.find({})
+        .populate('message')
+        .exec(function(error, list_messages) {
+            if(error) {
+                res.status(500).json({ result: false, message: "Cannot get message list", error: error }); 
+            }
+            res.status(200).json({result: true, message_list: list_messages} );
+        })
+}
--- Backend/routes/messageRouter.js
+++ Backend/routes/messageRouter.js
@@ -3,4 +3,6 @@ const router = express.Router();
 //const MongoClient = require('mongodb').MongoClient;
 const messageController = require("../controllers/messageController");
 
+router.get('/', messageController.getMessage);
+
 module.exports.messageRouter = router;
