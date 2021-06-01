9ef04ea51a1269e22d2b2a8bc711ee982c0449ee
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-01 22:36:47 +0300

    Refactor types

--- Backend/models/message.js
+++ Backend/models/message.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
-	text: {type: String},
-	chat: {type: Chat, required: true},
-	sender: {type: User, required: true},
-	dispatchDate: {type: Date, required: true},
+    text: { type: String },
+    chat: { type: String, required: true },
+    sender: { type: String, required: true },
+    dispatchDate: { type: Date, required: true },
 });
 
 module.exports = mongoose.model('Message', messageSchema);
