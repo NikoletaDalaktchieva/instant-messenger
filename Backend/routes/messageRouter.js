f13ee0a5b29d4bdd352ad5179557c3000b4c3551
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-01 22:20:41 +0300

    Created message router

---
+++ Backend/routes/messageRouter.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+//const MongoClient = require('mongodb').MongoClient;
+const messageController = require("../controllers/messageController");
+
+module.exports.messageRouter = router;
