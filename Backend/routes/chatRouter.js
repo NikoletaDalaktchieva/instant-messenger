4606f7a8b9c65775f9f162d3360b4eacfd0a35f3
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-01 22:38:32 +0300

    Tested loading list of chats

--- Backend/routes/chatRouter.js
+++ Backend/routes/chatRouter.js
@@ -3,4 +3,6 @@ const router = express.Router();
 //const MongoClient = require('mongodb').MongoClient;
 const chatController = require("../controllers/chatController");
 
+router.get('/', chatController.getChat);
+
 module.exports.chatRouter = router;
