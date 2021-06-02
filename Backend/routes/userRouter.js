944fc7ffdc4860cd3ea7f254323a367b5d67a8e9
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-02 23:52:34 +0300

    Moved the login post request to userController

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const router = express.Router();
-//const MongoClient = require('mongodb').MongoClient;
 const userController = require("../controllers/userController");
 
-
 router.post('/', userController.create);
-
-// //Check is user exist
-// router.post('/login', (request, respond) => {
-//   MongoClient.connect(process.env.DB_URL, function (err, db) {
-//     if (err) return respond.status(502).send();
-//     console.log("Database created!");
-//     var dbo = db.db(process.env.DB_NAME);
-//     dbo.collection(collection).findOne({ "user": request.body.user, "password": request.body.password }, function (err, res) {
-//       if (err) return respond.status(502).send();
-//       else if (res == null) respond.json({ result: 0, message: 'User not Found' });
-//       else return respond.json({ result: 1, id: res._id });
-//     });
-//   });
-// });
-
+router.post('/', userController.login);
 router.get('/', userController.getUsers);
 
 module.exports.userRouter = router;
-
