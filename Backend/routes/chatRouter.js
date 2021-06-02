c939530502dc717dca0776890253e9ff4cf1654d
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-02 23:19:02 +0300

    Deleted some useless declarations

--- Backend/routes/chatRouter.js
+++ Backend/routes/chatRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-//const MongoClient = require('mongodb').MongoClient;
 const chatController = require("../controllers/chatController");
 
 router.get('/', chatController.getChat);
