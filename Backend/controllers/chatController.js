7e5c210625c54a9cc256f04294172d9126986d94
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-02 23:18:06 +0300

    Formatted the document

--- Backend/controllers/chatController.js
+++ Backend/controllers/chatController.js
@@ -3,10 +3,10 @@ const Chat = require('../models/chat');
 exports.getChat = function (req, res) {
     Chat.find({})
         .populate('chat')
-        .exec(function(error, list_chats) {
-            if(error) {
-                res.status(500).json({ result: false, message: "Cannot get chat list", error: error }); 
+        .exec(function (error, list_chats) {
+            if (error) {
+                res.status(500).json({ result: false, message: "Cannot get chat list", error: error });
             }
-            res.status(200).json({result: true, chat_list: list_chats} );
+            res.status(200).json({ result: true, chat_list: list_chats });
         })
 }
