5304019871d99b0707c26aaeafe7a54f49b4d5af
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-02 23:54:31 +0300

    Moved the login post request to userController

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,3 +1,4 @@
+const { MongoClient } = require('mongodb');
 const User = require('../models/user');
 
 exports.create = function (req, res) {
@@ -17,13 +18,30 @@ exports.create = function (req, res) {
     );
 }
 
-exports.getUsers = function (req, res) {
+exports.login = function (req, res) {
+    MongoClient.connect(process.env.DB_URL, function (_err, db) {
+        console.log("Database created!");
+        const dbo = db.db(process.env.DB_NAME);
+        dbo.collection(collection).findOne({ "user": req.body.user, "password": req.body.password }, function (err, res) {
+            if (err) {
+                return res.status(500).send();
+            } else if (res == null) {
+                res.json({ result: false, message: 'User not Found' });
+            }
+            else {
+                return res.json({ result: true, id: res._id });
+            }
+        });
+    });
+}
+
+exports.getUsers = function (_req, res) {
     User.find({})
         .populate('user')
-        .exec(function(error, list_users) {
-            if(error) {
-                res.status(500).json({ result: false, message: "Cannot get user list", error: error }); 
+        .exec(function (error, list_users) {
+            if (error) {
+                res.status(500).json({ result: false, message: "Cannot get user list", error: error });
             }
-            res.status(200).json({result: true, user_list: list_users} );
+            res.status(200).json({ result: true, user_list: list_users });
         })
 }
