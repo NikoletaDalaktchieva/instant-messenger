09758ff2364a1ae2fa59d0090f4e4f38910c4bcb
Николета Далакчиева <nikoletadal@gmail.com>
2021-06-04 17:15:48 +0300

    .

--- Frontend/instant-messanger/src/app/services/user.service.ts
+++ Frontend/instant-messanger/src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Router } from '@angular/router';
 import { ErrorService } from "../services/error.service";
-import { AppComponent } from "./app.component";
+import { AppComponent } from "../app.component";
 import * as moment from "moment";
 
 @Injectable({
