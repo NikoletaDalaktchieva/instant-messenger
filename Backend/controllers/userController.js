5b08bfb841ac78c11fe6d8ddeef6429965d8bc88
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2021-06-04 15:46:58 +0300

    Refactored requirement

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/userSchema');
 
 exports.create = function (req, res) {
     User.init();
