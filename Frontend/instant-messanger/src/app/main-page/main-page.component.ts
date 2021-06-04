8b4f4fa136d2f8f9b6c0f39b1635e24619d080eb
Николета Далакчиева <nikoletadal@gmail.com>
2021-06-04 15:14:29 +0300

    .

--- Frontend/instant-messanger/src/app/main-page/main-page.component.ts
+++ Frontend/instant-messanger/src/app/main-page/main-page.component.ts
@@ -11,15 +11,9 @@ import { UserService } from "../services/user.service";
 })
 
 export class MainPageComponent implements OnInit {
-<<<<<<< HEAD
   chats;
   chatSearch = '';
   userName;
-=======
-  users = USERS;
-  title = "Undefine"
-  userSearch = '';
->>>>>>> develop
 
   constructor(private userService: UserService,
     private router: Router,
@@ -64,37 +58,13 @@ export class MainPageComponent implements OnInit {
       );
   }
 
-<<<<<<< HEAD
   openChat(chat: any) {
     this.chatService.setChatId(chat._id)
-=======
-  ngOnInit() {
-
-  
-
-    // var result;
-    // this.userService.load().
-    //   subscribe(
-    //     response => {
-    //       result = response;
-    //       console.log(result);
-    //       if (result.result === true) {
-    //         console.log(result.users);
-    //         this.users = result.users;
-    //       } else {
-    //         console.log(result.message);
-    //         AppComponent.showError(result.message);
-    //       }
-    //     },
-    //     error => { AppComponent.showError(); },
-    //     () => { }
-    //   );
   }
 
 
   getSettings(event) {
     alert("There is no such name in the history list!");
->>>>>>> develop
   }
 
   logout() {
