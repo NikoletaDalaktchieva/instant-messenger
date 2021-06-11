ca26d695c5f8f0e1e4afae0ab8ff68d793aeb917
deekay <daniel.2049kam@gmail.com>
2021-06-11 22:56:23 +0300

    small fix maybe

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -34,7 +34,7 @@ exports.login = function (req, res) {
                 }, 'scrt', {
                     expiresIn: '1h'
                 });
-		res.header("Access-Control-Allow-Origin", "*");
+		        res.header("Access-Control-Allow-Origin", "*");
                 res.json({ result: true, user: user, token: token });
             }
         });
--- Backend/index.js
+++ Backend/index.js
@@ -11,11 +11,10 @@ app.use(express.json());
 app.use(cors({
   origin: '*'
 }));
-app.use(function * (next) {
-    this.response.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    this.response.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    this.response.set('Access-Control-Allow-Origin', '*');
-    yield * next;
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
 });
 app.use('/user', userRouter);
 app.use(authMiddleware);
