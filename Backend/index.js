d4c75adb0ea32fd66ab3172db10d8f64f186a0e9
deekay <daniel.2049kam@gmail.com>
2021-06-11 23:24:56 +0300

    small fixes?

--- Backend/index.js
+++ Backend/index.js
@@ -8,14 +8,15 @@ const messageController = require('./controllers/messageController');
 const { authMiddleware } = require('./middleware/authMiddleware');
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
  next();
 });
+
 app.use('/user', userRouter);
 app.use(authMiddleware);
 app.use('/chat', chatRouter);
