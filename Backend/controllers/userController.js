080d841d8458c879b9c97f382b9f02309316b439
root <root@epimetheus.fmi.uni-sofia.bg>
2021-06-12 18:13:06 +0300

    password encryption fix

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,10 +1,10 @@
-import bcrypt from 'bcrypt';
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userSchema');
 
 const SALT_ROUNDS = 5; //cost factor
 
-exports.create = function (req, res) {
+exports.create = async function (req, res) {
     const hashedPass = await bcrypt.hash(req.body.password, SALT_ROUNDS);
     const user = new User({
         name: req.body.user,
@@ -23,16 +23,19 @@ exports.create = function (req, res) {
     );
 }
 
-exports.login = function (req, res) {
-    const hashedPass = await bcrypt.hash(req.body.password, SALT_ROUNDS);
-    User.findOne({ $or: [{ 'email': req.body.user }, { 'name': req.body.user }], 'password': hashedPass })
+exports.login = async function (req, res) {
+    User.findOne({ $or: [{ 'email': req.body.user }, { 'name': req.body.user }]})
         .populate('user')
-        .exec(function (err, user) {
+        .exec(async function (err, user) {
             if (err) {
                 res.status(500).send();
             } else if (user == null) {
                 res.json({ result: false, message: 'User not Found' });
             } else {
+		
+		const pass = await bcrypt.compare(req.body.password, user.password);
+		if(pass){
+
                 const token = jwt.sign({
                     id: user.id,
                     name: user.name,
@@ -41,6 +44,9 @@ exports.login = function (req, res) {
                 });
 		        res.header("Access-Control-Allow-Origin", "*");
                 res.json({ result: true, user: user, token: token });
+		} else {
+		res.json({ result: false, message: 'User not Found' });
+		}
             }
         });
 }
