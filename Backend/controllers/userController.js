988f2621e5569cdbe5b706f4ef6a5e45ab24dbc4
deekay <daniel.2049kam@gmail.com>
2021-06-13 16:51:05 +0300

    fixed commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,11 +1,15 @@
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userSchema');
 
-exports.create = function (req, res) {
+const SALT_ROUNDS = 5; //cost factor
+
+exports.create = async function (req, res) {
+    const hashedPass = await bcrypt.hash(req.body.password, SALT_ROUNDS);
     const user = new User({
         name: req.body.user,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPass
     });
     user.save().then(
         (createdUser) => {
@@ -19,22 +23,30 @@ exports.create = function (req, res) {
     );
 }
 
-exports.login = function (req, res) {
-    User.findOne({ $or: [{ 'email': req.body.user }, { 'name': req.body.user }], 'password': req.body.password })
+exports.login = async function (req, res) {
+    User.findOne({ $or: [{ 'email': req.body.user }, { 'name': req.body.user }]})
         .populate('user')
-        .exec(function (err, user) {
+        .exec(async function (err, user) {
             if (err) {
                 res.status(500).send();
             } else if (user == null) {
                 res.json({ result: false, message: 'User not Found' });
             } else {
+		
+		const pass = await bcrypt.compare(req.body.password, user.password);
+		if(pass){
+
                 const token = jwt.sign({
                     id: user.id,
                     name: user.name,
                 }, 'scrt', {
                     expiresIn: '1h'
                 });
+		        res.header("Access-Control-Allow-Origin", "*");
                 res.json({ result: true, user: user, token: token });
+		} else {
+		res.json({ result: false, message: 'User not Found' });
+		}
             }
         });
 }
